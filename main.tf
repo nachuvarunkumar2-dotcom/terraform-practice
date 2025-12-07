10b254ed3ec396a01c033df9f2ed74d94d8fe930
Varun Kumar <nachuvarunkumar2@gmail.com>
2025-12-07 18:40:27 -0500

    Use SPN auth for azurerm provider

--- main.tf
+++ main.tf
@@ -7,13 +7,13 @@ terraform {
   }
 }
 
-provider "azurerm" {
-  features {}
+#provider "azurerm" {
+#  features {}
 
   # Tell Terraform exactly which Azure subscription + tenant to use
-  subscription_id = "a25cecd1-bf85-4c40-a891-fafe0e83271d"
-  tenant_id       = "7508ef84-f789-4799-a1b1-3fa89645b91c"
-}
+  # subscription_id = "a25cecd1-bf85-4c40-a891-fafe0e83271d"
+  # tenant_id       = "7508ef84-f789-4799-a1b1-3fa89645b91c"
+# }
 
 # Create Resource Group in Azure
 resource "azurerm_resource_group" "example" {
