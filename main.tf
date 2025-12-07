6d7d282ce64b6323acc61b6fbb1702f1116cf791
Varun Kumar <nachuvarunkumar2@gmail.com>
2025-12-07 18:23:37 -0500

    Clean Terraform project with workflow

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  # Tell Terraform exactly which Azure subscription + tenant to use
+  subscription_id = "a25cecd1-bf85-4c40-a891-fafe0e83271d"
+  tenant_id       = "7508ef84-f789-4799-a1b1-3fa89645b91c"
+}
+
+# Create Resource Group in Azure
+resource "azurerm_resource_group" "example" {
+  name     = "varun-tf-rg"
+  location = "East US"
+}
