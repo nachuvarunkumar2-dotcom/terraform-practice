315db0335b46cdc4fef6fa59dcdc347ea9e7f795
Varun Kumar <nachuvarunkumar2@gmail.com>
2025-12-08 14:45:05 -0500

    Updated main.tf with new Azure provider and resource group configs

--- main.tf
+++ main.tf
@@ -1,23 +1,37 @@
 terraform {
+  required_version = ">= 1.0.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0"
+      version = ">= 4.0"
     }
   }
 }
 
 provider "azurerm" {
-   features {}
-
-  # Tell Terraform exactly which Azure subscription + tenant to use
+  features {}
+  # By default this will use your Azure CLI login (az login).
   # subscription_id = "a25cecd1-bf85-4c40-a891-fafe0e83271d"
-  # tenant_id       = "7508ef84-f789-4799-a1b1-3fa89645b91c"
- }
-
-# Create Resource Group in Azure
-resource "azurerm_resource_group" "example" {
-  name     = "varun-tf-rg2"
-  location = "west US"
+  #  tenant_id       = "7508ef84-f789-4799-a1b1-3fa89645b91c"
 }
 
+variable "rg_name" {
+  type        = string
+  description = "Name of the resource group to create"
+  default     = "varun-tf-rg"
+}
+
+variable "location" {
+  type        = string
+  description = "Azure location for resources"
+  default     = "eastus"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = var.rg_name
+  location = var.location
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.example.name
+}
